c60eb798d6db45094b87c0658da5b0724e91845e
Kevin Reynolds <kevin@sensationmultimedia.co.uk>
2025-03-12 12:28:47 -0700

    restore cache

--- src/utils/fetchGraphQL.ts
+++ src/utils/fetchGraphQL.ts
@@ -25,7 +25,7 @@ export async function fetchGraphQL<T = any>(
           ...headers,
         },
         body,
-        cache: "no-cache",
+        cache: "default",
         next: {
           tags: ["wordpress"],
         },
